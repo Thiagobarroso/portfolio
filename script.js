05c756004f7149156a0445a4a66d9d0c62c37330
Thiagobarroso <thiagoo.silva@live.com>
2024-05-19 01:50:34 -0300

    adicionando botão de retonar ao inicio

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const selecionarMenuMobile = document.querySelector('.menu-hamburguer-mobile');
 menuHambuguer.addEventListener('click', function () {
   selecionarMenuMobile.classList.toggle('ativo');
 });
+
+
+const getHeader = document.querySelector('header')
+const getSeta = document.querySelector(".quadrado-seta")
+
+window.onscroll = function(){
+   let distanciaTopo = getHeader.getBoundingClientRect().top;
+   console.log(distanciaTopo)
+   
+   if (distanciaTopo >= -250){
+    getSeta.style.display = 'none'
+   } else {
+    getSeta.style.display = 'block'
+   }
+   }
+
